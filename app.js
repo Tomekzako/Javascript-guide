5898b29a790d10d17a8d46846775b28c7e0b16c3
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-05-09 14:20:25 +0200

    Practice

--- app.js
+++ app.js
@@ -537,8 +537,22 @@ function returnFunction() {
         }
     }
 
-    Question.prototype.checkAnswer = function (ans) {
-        ans == this.correct ? console.log("Correct answer!") : console.log("Wrong answer. Try again :)");
+    Question.prototype.checkAnswer = function (ans, callback) {
+        let sc;
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            sc = callback(true);
+
+        } else {
+            console.log('Wrong answer. Try again :)');
+            sc = callback(false);
+        }
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function (score) {
+        console.log(`Your current score is ${score}`);
+        console.log('----------------------');
     }
 
 
@@ -547,6 +561,18 @@ function returnFunction() {
     const q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
 
     const questions = [q1, q2, q3];
+
+    function score() {
+        let sc = 0;
+        return function (correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    const keepscore = score();
 
     function nextQuestion() {
 
@@ -557,12 +583,12 @@ function returnFunction() {
         const answer = prompt('Please select the correct answer.');
 
         if (answer !== 'exit') {
-            questions[randomQuestion].checkAnswer(parseInt(answer));
+            questions[randomQuestion].checkAnswer(parseInt(answer), keepscore);
             nextQuestion();
         }
     }
 
-        nextQuestion();
+    nextQuestion();
 
 })();
 
